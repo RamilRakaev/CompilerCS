881c9bcf4d03d4d7a0d690c9a7871bce8622a144
RamilRakaev <59777361+RamilRakaev@users.noreply.github.com>
2021-01-22 13:56:01 +0300

    Добавьте файлы проекта.

---
+++ CompilerCS/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CompilerCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.FrameworkTBox = new System.Windows.Forms.TextBox();
+            this.CodeBox = new System.Windows.Forms.RichTextBox();
+            this.statusBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Версия Framework";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выходной файл";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(133, 58);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(341, 22);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // FrameworkTBox
+            // 
+            this.FrameworkTBox.Location = new System.Drawing.Point(625, 58);
+            this.FrameworkTBox.Name = "FrameworkTBox";
+            this.FrameworkTBox.Size = new System.Drawing.Size(100, 22);
+            this.FrameworkTBox.TabIndex = 3;
+            // 
+            // CodeBox
+            // 
+            this.CodeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CodeBox.Font = new System.Drawing.Font("Consolas", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CodeBox.Location = new System.Drawing.Point(71, 133);
+            this.CodeBox.Name = "CodeBox";
+            this.CodeBox.Size = new System.Drawing.Size(658, 142);
+            this.CodeBox.TabIndex = 4;
+            this.CodeBox.Text = "";
+            // 
+            // statusBox
+            // 
+            this.statusBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusBox.Font = new System.Drawing.Font("Consolas", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusBox.Location = new System.Drawing.Point(71, 333);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(658, 105);
+            this.statusBox.TabIndex = 5;
+            this.statusBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(630, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.CodeBox);
+            this.Controls.Add(this.FrameworkTBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CompolerCS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox FrameworkTBox;
+        private System.Windows.Forms.RichTextBox CodeBox;
+        private System.Windows.Forms.RichTextBox statusBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CompilerCS/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+namespace CompilerCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CodeBox.Text= @"using System;
+namespace Test
+{
+class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(""Hello, world!"");
+                Console.ReadKey();
+            }
+        }
+    }
+";
+            nameTextBox.Text = "App.exe";
+            FrameworkTBox.Text = "v4.0";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider
+                (new Dictionary<string, string>() { { "CompilerVersion", FrameworkTBox.Text } });
+
+            CompilerParameters parameters = new CompilerParameters
+                (new string[] { "mscorlib.dll", "System.Core.dll" }, nameTextBox.Text, true);
+            parameters.GenerateExecutable = true;
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, CodeBox.Text);
+            if (results.Errors.HasErrors)
+            {
+                foreach (CompilerError error in results.Errors.Cast<CompilerError>())
+                {
+                    statusBox.Text += $"Строка{error.Line}: {error.ErrorText}";
+                }
+            }
+            else
+            {
+                statusBox.Text = "---Сборка завершена---";
+                Process.Start($"{Application.StartupPath}/{nameTextBox.Text}");
+            }
+
+            }
+        }
+    }
+
